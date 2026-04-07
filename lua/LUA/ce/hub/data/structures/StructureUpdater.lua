d985677f13de2d069c948f3b0c7f66128d4a22a2
Andreas Kreuz <33174238+Andreas-Kreuz@users.noreply.github.com>
2026-04-07 20:27:23 +0200

    Restrict structure detection to initial discovery

--- lua/LUA/ce/hub/data/structures/StructureUpdater.lua
+++ lua/LUA/ce/hub/data/structures/StructureUpdater.lua
@@ -5,46 +5,16 @@ local SyncPolicy = require("ce.hub.sync.SyncPolicy")
 
 local StructureUpdater = {}
 
-local EEPStructureGetPosition = _G.EEPStructureGetPosition or function () end
-local EEPStructureGetRotation = _G.EEPStructureGetRotation or function () end
-local EEPStructureGetModelType = _G.EEPStructureGetModelType or function () end
 local EEPStructureGetLight = _G.EEPStructureGetLight or function () end
 local EEPStructureGetSmoke = _G.EEPStructureGetSmoke or function () end
 local EEPStructureGetFire = _G.EEPStructureGetFire or function () end
 local EEPStructureGetTagText = _G.EEPStructureGetTagText or function () end
-
-local EEPStructureModelTypeText = {
-    [16] = "Gleis/Gleisobjekt",
-    [17] = "Schiene/Gleisobjekt",
-    [18] = "Strasse/Gleisobjekt",
-    [19] = "Sonstiges/Gleisobjekt",
-    [22] = "Immobilie",
-    [23] = "Landschaftselement/Fauna",
-    [24] = "Landschaftselement/Flora",
-    [25] = "Landschaftselement/Terra",
-    [38] = "Landschaftselement/Instancing"
-}
-
-local function round2(value)
-    return value and tonumber(string.format("%.2f", value)) or 0
-end
-
-local function applyStaticUpdate(structure)
-    local _, modelType = EEPStructureGetModelType(structure.name)
-    local _, posX, posY, posZ = EEPStructureGetPosition(structure.name)
-    local _, rotX, rotY, rotZ = EEPStructureGetRotation(structure.name)
-
-    structure:setModelType(modelType or 0, EEPStructureModelTypeText[modelType] or "")
-    structure:setPosition(round2(posX), round2(posY), round2(posZ))
-    structure:setRotation(round2(rotX), round2(rotY), round2(rotZ))
-end
 
 function StructureUpdater.runInitialUpdate()
     local HubOptionsRegistry = require("ce.hub.options.HubOptionsRegistry")
     if not HubOptionsRegistry.isDiscoveryAndUpdateEnabled("structures") then return end
 
     for _, structure in pairs(StructureRegistry.getAll()) do
-        applyStaticUpdate(structure)
         structure:resetDirty()
     end
 end
@@ -59,7 +29,6 @@ function StructureUpdater.runUpdate()
     for _, structure in pairs(StructureRegistry.getAll()) do
         local isSelected = DynamicUpdateRegistry.isSelected(HubCeTypes.Structure,
                                                             tostring(structure.id or structure.name))
-        applyStaticUpdate(structure)
         if SyncPolicy.shouldUpdateField(fields, "tag", isSelected) then
             local _, tag = EEPStructureGetTagText(structure.name)
             structure:setTag(tag or "")
